71cd5afbdbeea803dd49efce116bcaa11efddf4d
irismonadesign <irismonadesign@gmail.com>
2021-01-19 16:11:05 +0900

    first source

---
+++ src/main/groovy/com/sample/jerry/JerryApplication.groovy
@@ -0,0 +1,13 @@
+package com.sample.jerry
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JerryApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JerryApplication, args)
+    }
+
+}
